875effb8b6a83b6098cc1dafd9be3cc58662a803
Muhammad Ramadhan <mr.maulanaasyarief@gmail.com>
2021-06-29 12:01:04 +0700

    Finished Class Schedule

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random as rnd
+
 class Data:
     ROOMS = [["R1", 25], ["R2", 45], ["R3", 35]]
     MEETING_TIMES = [["MT1", "MWF 09.00 - 10.00"],
@@ -41,7 +43,52 @@ class Data:
     def get_depts(self): return self._depts
     def get_numberOfClasses(self): return self._numberOfClasses
 class Schedule:
-    ''' '''
+    def __init__(self):
+        self._data = data
+        self._classes = []
+        self._numbOfConflicts = 0
+        self._fitness = -1
+        self._classNumb = 0
+        self._isFitnessChanged = True
+    def get_classes(self):
+        self._isFitnessChanged = True
+        return self._classes
+    def get_numbOfConflicts(self): return self._numbOfConflicts
+    def get_fitness(self):
+        if(self._isFitnessChanged == True):
+            self._fitness = self.calculate_fitness()
+            self._isFitnessChanged = False
+        return self._fitness
+    def initialize(self):
+        depts = self._data.get_depts()
+        for i in range(0, len(depts)):
+            courses = depts[i].get_courses()
+            for j in range(j, len(courses)):
+                newClass = Class(self._classNumb, depts[i], courses[j])
+                self._classNumb += 1
+                newClass.set_meetingTime(data.get_meetingTimes()[rnd.randrange(0, len(data.get_meetingTimes()))])
+                newClass.set_room(data.get_rooms()[rnd.randrange(0, len(data.get_rooms()))])
+                newClass.set_instructor(data.get_instructors()[rnd.randrange(0, len(data.get_instructors()))])
+                self._classes.append(newClass)
+        return self
+    def calculate_fitness(self):
+        self._numbOfConflicts = 0
+        classes = self.get_classes()
+        for i in range(0, len(classes)):
+            if(classes[i].get_room().get_seatingCapacity() < classes[i].get_course().get_maxNumbOfStudents()):
+                self._numbOfConflicts += 1
+            for j in range(0, len(classes)):
+                if(j >= i):
+                    if(classes[i].get_meetingTime() == classes[j].get_meetingTime() and classes[i].get_id() != classes[j].get_id()):
+                        if(classes[i].get_room() == classes[j].get_room()): self._numbOfConflicts += 1
+                        if(classes[i].get_instructor() == classes[j].get_instructor): self._numbOfConflicts += 1
+        return 1 / ((1.0*self._numbOfConflicts + 1))
+    def __str__(self):
+        returnValue = ""
+        for i in range(0, len(self._classes)-1):
+            returnValue += str(self._classes[i])+ "; "
+        returnValue += str(self._classes[len(self._classes)-1])
+        return returnValue
 class Population:
     ''' '''
 class GeneticAlgorithm:
@@ -102,3 +149,5 @@ class Class:
     def __str__(self): 
         return str(self._dept.get_name()) + "," + str(self._course.get_number()) + "," + \
                str(self._room.get_number()) + "," + str(self._instructor.get_id()) + "," + str(self._meetingTime.get_id())
+
+data = Data()
